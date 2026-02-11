7cd9bcdd2c2412630f8b1ae53869dde1971e62e2
Baljeet <baljeet@Baljeets-MacBook-Air.local>
2026-02-11 15:38:08 +0530

    11-02-2026

--- js/script.js
+++ js/script.js
@@ -1,105 +1,160 @@
-const gameCanvas = document.querySelector("#gameCanvas");
-gameCanvas.width = window.innerWidth;
-gameCanvas.height = window.innerHeight;
-gameCanvas.style.background = "yellow";
+//1. Project Setup
+//Html->canvas, javascript reference, canvas basic,
+//obtain the context
+//2. Player
+//3. Player Movement ->y
+//4. Gravity
+//5. Full Movements left->right->up->down(already)
+//6. Screen Restrict
+//7. Platform
 
-const context = gameCanvas.getContext("2d");
-context.fillStyle = "red";
+const gravity=0.5;
+const speed=2;
+const gameCanvas=document.querySelector("#gameCanvas");
+gameCanvas.width=window.innerWidth;
+gameCanvas.height=window.innerHeight;
+gameCanvas.style.background="yellow";
+const keys={
+    right:false,
+    left:false
+}
+const context=gameCanvas.getContext("2d");
 
-// context.fillRect(100,100,50,100);
-// context.fillRect(200,100,50,100);
-// context.strokeRect(300,100,50,100);
-// context.beginPath();
-
-// context.arc(100,100,50,0,Math.PI*2);
-// context.moveTo(200,200);
-// context.lineTo(200,50);
-// context.lineTo(300,300);
-
-
-
-// context.stroke();
-// context.closePath();
-
-// context.font="50px Arial"
-// context.fillText("Hello",200,200);
-// context.strokeText("Hello",300,200);
-
-class Circle {
-    x = 10;
-    constructor(x, y, radius,speed) {
-        this.x = x;
-        this.y = y;
-        this.radius = radius;
-        this.speed=speed;
+class Player{
+    constructor(){
+        this.position={
+            x:150,
+            y:450
+        }
+        this.velocity={
+            x:0,
+            y:speed
+        }
+        this.width=20;
+        this.height=20;
     }
-    // display(x, y, radius) {
-    //     this.x = x;
-    //     this.y = y;
-    //     this.radius = radius;
-    //     //console.log(this.x);
+    draw()
+    {
+        context.fillStyle="black";
+        context.fillRect(this.position.x,this.position.y,this.width,this.height);
 
-    // }
-    draw() {
-        context.beginPath();
-        context.arc(this.x+this.speed, this.y+this.speed, this.radius, 0, Math.PI * 2);
-        context.stroke();
-        context.closePath();
+    }
+    update()
+    {
+        this.position.y+=this.velocity.y;
+        this.position.x+=this.velocity.x;
+        if(this.position.y+this.height+this.velocity.y>=gameCanvas.height)
+            this.velocity.y=0;
+        else
+        {
+            this.velocity.y+=gravity;
+
+        }
+        
+
+        this.draw();
     }
 }
-//Circle c; Compile time
-//Circle *c=new Circle(); run Time
-//Java ->Runtime
-//Circle c=new Circle();
-// let c = new Circle(100,100,50);
-// //c.display(100,100,50);
-// c.draw();
+class Platform{
+    constructor(x,y,width,height)
+    {
+        this.position={
+            x:x,
+            y:y
+        }
+        this.width=width;
+        this.height=height;
 
-// let c1 = new Circle(200,200,50);
-// //c1.display(200,200,50);
-// c1.draw();
-//c.display();
+    }
+    draw()
+    {
+            context.fillStyle="red";
+            context.fillRect(this.position.x,this.position.y,this.width,this.height);
 
-// let circles=[];
-// for(i=1;i<=10;i++)
-// {
-//    // let c=new Circle(100+i*10,100+i*10,50);
+    }
+}
+const player=new Player();
+player.draw();
 
-//    let x=Math.random()*window.innerWidth-50;
-//    let y=Math.random()*window.innerHeight;
-//    let c=new Circle(x,y,50);
-
-//     circles.push(c);
-// }
-
-// circles.forEach((item)=>{
-//     item.draw();
-
-// })
-
-let c=new Circle(100,100,50,5);
-c.draw();
-
-// console.log("First");
-// setTimeout(test,0);
-// console.log("Second");
-// function test()
-// {
-//     console.log("Test called");
-// }
-//setInterval(animate,100);
+const platform=new Platform(350,gameCanvas.height-100,40,100);
+//  const platform=new Platform(350,gameCanvas.height-150,140,20);
+//platform.draw();
 
 function animate()
 {
     requestAnimationFrame(animate);
-
     context.clearRect(0,0,window.innerWidth,window.innerHeight);
+    
+platform.draw();
+    player.update();
+    
+    if(keys.right && player.position.x<=950)
+        player.velocity.x=speed;
+    else if(keys.left && player.position.x>=150)
+        player.velocity.x=-speed;
+    else
+        player.velocity.x=0;
 
-    c.speed+=1;
+    if(
+        player.position.x+player.width>=platform.position.x 
+        && player.position.x+player.width<=platform.position.x+platform.width
+        && player.position.y<=platform.position.y+platform.height
+    && player.position.y+player.height>=platform.position.y
+)
+        player.velocity.x=0;
 
-    c.draw();
+            if( player.position.x+player.width>=platform.position.x 
+        && player.position.x<=platform.position.x+platform.width
+       &&
+                player.position.y+player.height+player.velocity.y>=platform.position.y
+            
+            &&
+            player.position.y+player.height<=platform.position.y
+            )
+                player.velocity.y=0;
+
 }
 
-animate();
+// addEventListener("keydown",(e)=>{
+// //console.log(e);
+// if(e.key=="ArrowRight")
+//     player.velocity.x=speed;
+// if(e.key=="ArrowLeft")
+//     player.velocity.x=-speed;
+// if(e.key=="ArrowUp")
+//     player.velocity.y=-12;
+// })
 
+// addEventListener("keyup",(e)=>{
+// //console.log(e);
+// if(e.key=="ArrowRight")
+//     player.velocity.x=0;
+// if(e.key=="ArrowLeft")
+//     player.velocity.x=0;
+// })
+
+
+addEventListener("keydown",(e)=>{
+//console.log(e);
+if(e.key=="ArrowRight")
+    keys.right=true;
+    //player.velocity.x=speed;
+if(e.key=="ArrowLeft")
+    keys.left=true;
+   // player.velocity.x=-speed;
+if(e.key=="ArrowUp")
+    player.velocity.y=-12;
+})
+
+addEventListener("keyup",(e)=>{
+//console.log(e);
+if(e.key=="ArrowRight")
+    keys.right=false;
+   // player.velocity.x=0;
+if(e.key=="ArrowLeft")
+    keys.left=false;
+    //player.velocity.x=0;
+})
+
+animate();
 
